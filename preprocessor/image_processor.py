ff1c1416d169a0ef0dd78638f1c5f354a308c127
Lyna Hocine <lyna.hocine@etu.sorbonne-universite.fr>
2026-03-02 13:03:14 +0100

    image_preprocessor

--- preprocessor/image_processor.py
+++ preprocessor/image_processor.py
@@ -1,4 +1,25 @@
-def image_to_text(image_bytes: bytes) -> str:
-    """Calls a vision model to turn the image into a text prompt"""
+import requests
+import base64
 
-    return 
+def image_to_base64(url : str) -> str:
+    """Fetch image from url and return base64 data uri string"""
+
+    #send get request to retrieve the image
+    response = requests.get(url, timeout=10)
+
+    #if the request is successful
+    if response.status_code == 200:
+        #verify file type 
+        content_type = response.headers.get("Content-Type","")
+
+        if not content_type.startswith("image/"):
+            raise Exception(f"URL does not point to an image. Content-type : {content_type}")
+        
+        #encode image into Base64 string
+        base64_str = base64.b64encode(response.content).decode('utf-8')
+        #return properly formatted data URI for multimodal LLM usage
+        return f"data:{content_type};base64,{base64_str}"
+    
+    else: 
+        raise Exception(f"Failed to fetch image :{response.status_code}")
+    
--- tool/web_search_tool.py
+++ tool/web_search_tool.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import urllib.parse
 
 def web_search_tool(query) -> dict:
     """It takes the user query (text) as input.
@@ -12,22 +13,32 @@ def web_search_tool(query) -> dict:
     
     #web tool does search with DuckDuckGo
 
-    url = f"https://html.duckduckgo.com/html/?q={q}"
-    res = requests.get(url)
+    encoded_q = urllib.parse.quote(q)
+    url = f"https://html.duckduckgo.com/html/?q={encoded_q}"
+
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    try:
+        res = requests.get(url, headers=headers, timeout=10)
+        res.raise_for_status()
+    except Exception as e:
+        return {"query": q, "result": f"Request failed: {str(e)}"}
     
     #extract the data from the html
     soup = BeautifulSoup(res.text, "html.parser")
 
     #get the top result 
-    results = soup.find_all("div", class_="result")
+    results = soup.find_all("a", class_="result__a")
 
     if not results: 
-        return {"query":q, "result": "No results found"}
+        return {"query":q, "result": "No results found (possibly blocked)"}
     
-    top = results[0]
+    top = results[0].get_text()
     title = (top.find("a", class_="result__a")).get_text()
 
-    snippet_tag = top.find("a", class_="result__snippet") or top.find("div", class_="result__snippet")
+    snippet_tag = top.find_next("a", class_="result__snippet") or top.find("div", class_="result__snippet")
     snippet = snippet_tag.get_text() if snippet_tag else "No snippet available"
 
     result = f"{title}\n{snippet}"
