e16ce3cf598c4a1845dd82f959eec3be71395a86
Lyna Hocine <lyna.hocine@etu.sorbonne-universite.fr>
2026-03-10 22:27:33 +0100

    some bugs correction

--- agent/agent.py
+++ agent/agent.py
@@ -1,9 +1,14 @@
 import asyncio
+import re
 import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from preprocessor import image_processor
 from langchain_ollama import ChatOllama
 from langchain_core.messages import HumanMessage, AIMessage, ToolMessage
 from langchain_mcp_adapters.client import MultiServerMCPClient
-from langgraph.prebuilt import create_react_agent
+from langgraph.prebuilt.chat_agent_executor import create_react_agent
 
 
 model = ChatOllama(model="gpt-oss:20b", temperature=0)
@@ -15,7 +20,7 @@ async def run_agent():
             "command" : "python",
             "args" : ["../server/server.py"],
             "transport" : "stdio",
-
+            "cwd": "../server",
         }
     })
         
@@ -25,9 +30,25 @@ async def run_agent():
     while True: 
         print("ask question: ", end="", flush=True)
         user_input = input()
+        
+        #searching for image url in the message
+        image_url_match = re.search(r'https?://\S+\.(?:png|jpg|jpeg)', user_input)
 
+        if image_url_match:
+            image_url = image_url_match.group()
+            base64_image = image_processor.ImagePreprocessor.image_to_base64(image_url)
+
+            #remove the url from the text to keep only the question
+            text = user_input.replace(image_url,"").strip()
+
+            message = HumanMessage(content=[
+                {"type": "image_url", "image_url": {"url": base64_image}},
+                {"type": "text", "text": text}
+            ])
+        else: 
+            message = HumanMessage(content = user_input)
         response = await agent.ainvoke({
-            "messages" : [HumanMessage (content = user_input)]
+            "messages" : [message]
         })
 
         for message in response["messages"]:
